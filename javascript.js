8af20933f338c69a3594a4eac072f0fef9ea7efd
Cyan Thayn <git@thayn.me>
2020-04-18 19:22:42 -0500

    Cleaned up & organized js & css

---
+++ javascript.js
@@ -0,0 +1,28 @@
+window.onload = function load() {
+  showSlides();
+
+  setInterval(() => {
+    showSlides();
+  }, 2500);
+
+}
+
+function showSlides() {
+  var randomNumber = Math.floor(Math.random() * 3) + 1;
+  document.getElementById("slides").src = `./media/landing/${randomNumber}.jpg`;
+}
+
+function mobiMenuOpen() {
+  document.getElementById("mobiMenu").style.display = "flex";
+  document.getElementById("mobiMenu").style.float = "left";
+  document.getElementById("mobiMenu").style.position = "absolute";
+  document.getElementById("mobiMenu").style.zIndex = "99 !important";
+  document.getElementById("mobiMenu").style.animationName = "mobiMenuOpen";
+}
+
+function mobiMenuClose() {
+  document.getElementById("mobiMenu").style.animationName = "mobiMenuClose";
+  setTimeout(() => {
+    document.getElementById("mobiMenu").style.display = "none";
+  }, 990);
+}

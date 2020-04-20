12c0a367734fd37fffdecc57585290c9e074e7a1
jchrestman2 <64033718+jchrestman2@users.noreply.github.com>
2020-04-20 13:54:09 -0500

    Update action_page.php
    updated servername, username, password, and dbname

--- action_page.php
+++ action_page.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "scaduser";
+$password = "GoAsim1";
+$dbname = "green-tomatoes";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);

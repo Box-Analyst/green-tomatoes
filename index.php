e1745174404fb4dc72f2072b97901c622b9e16bc
jchrestman2 <64033718+jchrestman2@users.noreply.github.com>
2020-04-20 16:20:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
   session_start(); 
 
   if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
+  	$_SESSION['msg'] = "You must log in to reserve a room";
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
